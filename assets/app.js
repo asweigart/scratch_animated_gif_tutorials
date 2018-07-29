1bcd83b81206bc87b2d47689539d861b315f0a48
Alex Riviere <fimion@gmail.com>
2018-07-28 20:09:56 -0400

    generate is working correctly, updating json format

--- assets/app.js
+++ assets/app.js
@@ -3,8 +3,9 @@
 (function(){
 
   var TutorialPage = {
-    el:"#tutorial-page",
-    data:function(){
+    template:"#tutorial-page",
+    delimiters:["(#",'#)'],
+    /*data:function(){
       return{
         slide:{
           title:"",
@@ -13,23 +14,65 @@
           foot:"",
         }
       }
-    },
-    created:function(){
-      var current_slide = window.tutorial.slides[this.$route.index]
-      if(current_slide){
-        this.slide = current_slide
+    },*/
+    computed:{
+      slide:function(){
+        var slide = this.slides[this.$route.params.index]
+        return slide?slide:this.slides[0]
       }
-    }
+    },
+    props:['slides'],
   }
 
   var router = new VueRouter({
     routes:[
-      {path:"/:index",component:TutorialPage}
+      {path:"/:index", name:"slide", component:TutorialPage},
+      {path:"/",redirect:"/0"}
     ]
   })
 
   new Vue({
     el:'#app',
+    delimiters:["(#",'#)'],
     router:router,
+    data:{
+      tutorial:window.tutorial
+    },
+    computed:{
+      slides:function(){
+        return this.tutorial.slides?this.tutorial.slides:[]
+      }
+    },
+    methods: {
+      changePage: function (e) {
+        var page = e.target.value;
+        this.$router.push({name: "slide", params: {index: page}})
+      },
+      nextPage: function () {
+        var next_page = parseInt(this.$router.currentRoute.params.index) + 1
+        if (next_page < this.slides.length) {
+          this.$router.push({name: "slide", params: {index: next_page}})
+        }
+      },
+      prevPage: function () {
+        var prev_page = parseInt(this.$router.currentRoute.params.index) - 1
+        if (prev_page >= 0) {
+          this.$router.push({name: "slide", params: {index: prev_page}})
+        }
+      },
+      arrowHandler:function(e){
+        switch(e.key){
+          case "ArrowRight":
+            this.nextPage()
+            break;
+          case "ArrowLeft":
+            this.prevPage()
+            break;
+        }
+      }
+    },
+    mounted:function(){
+      document.onkeyup = this.arrowHandler
+    }
   })
 })()
---
+++ generateVueTutorials.py
@@ -0,0 +1,59 @@
+import json
+import os
+import re
+import base64
+
+from pathlib import Path
+
+scriptLocation = os.path.dirname(os.path.realpath(__file__))
+TUTORIAL_JSON_FILES = [Path(scriptLocation) / 'archery' / 'archery.vue.json']
+
+indexTemplate = open(Path(scriptLocation) / 'assets' / 'tutorial.html').read()
+bulmaCss = open(Path(scriptLocation) / 'assets' / 'bulma.min.css').read()
+vueScript = open(Path(scriptLocation) / 'assets' / 'vue.dev.js').read()
+vueRouterScipt = open(
+    Path(scriptLocation) / 'assets' / 'vue-router.min.js').read()
+tutorialApp = open(Path(scriptLocation) / 'assets' / 'app.js').read()
+
+checkboxRegex = re.compile(r'\[(.*?)\]\(checkbox\)')
+markdownLinkRegex = re.compile(r'\[(.*?)\]\((.*?)\)')
+
+for jsonFile in TUTORIAL_JSON_FILES:
+
+    with open(jsonFile) as fo:
+        content = fo.read()
+        tutorialData = json.loads(content)
+        json_path = Path(os.path.dirname(jsonFile))
+
+        for index, slide in enumerate(tutorialData['slides']):
+            if slide.get('image', None):
+                with open(json_path / slide['image']['file'], 'rb') as fb:
+                    data = base64.b64encode(fb.read())
+                    tutorialData['slides'][index]['image']['src'] = \
+                        "data:image/gif;base64," + str(data, 'utf-8')
+
+            tutorialData['slides'][index]['text'] = ('<p>'
+                                                     + '</p><p>'.join(
+                        slide['text'].split('\n')
+                    )
+                                                     + '</p>'
+                                                     )
+            if slide.get('foot', None):
+                tutorialData['slides'][index]['foot'] = ('<p>'
+                                                         + '</p><p>'.join(
+                            slide['foot'].split('\n')
+                        )
+                                                         + '</p>'
+                                                         )
+
+        finalFile = indexTemplate.format(tutorial_title=tutorialData['name'],
+                                         bulma=bulmaCss,
+                                         tutorial_json=tutorialData,
+                                         vuejs=vueScript,
+                                         vuerouter=vueRouterScipt,
+                                         appjs=tutorialApp)
+        pageHtmlFilename = os.path.join(os.path.dirname(jsonFile),
+                                        '%s.html' % (tutorialData[
+                                                         'name'].lower()))
+        with open(pageHtmlFilename, 'w') as fo:
+            fo.write(finalFile)
